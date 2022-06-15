3529c39135e72d80087ce91b7cb58ef16a52d328
jad <jads@microsoft.com>
2022-06-15 21:28:23 +0000

    ssss

--- Load_Data.py
+++ Load_Data.py
@@ -2,6 +2,6 @@ import os
 import pandas as pd
 
 def Load_HDFS_Data(logfile):
-    df = pd.read_csv(logfile, engine='c', na_filter=False, memory_map=True, dtype={'Date':object, "Time": object})
+    df = pd.read_csv(logfile, engine='c', na_filter=False, memory_map=False, dtype={'Date':object, "Time": object})
     return df
 
